60b36c99fef2e8f2840027ae86ba8103f5226258
Jayjoshi798 <jayjoshi7901@gmail.com>
2025-08-28 21:17:14 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter your first number");
+    scanf("%d",&a);
+
+    printf("Enter your second number");
+    scanf("%d",&b);
+    
+    int c = a*b;
+    
+    printf("your ans is:%d",c);
+    return 0 ;
+    
+}
